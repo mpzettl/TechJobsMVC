e387ad75d70c4de5c108cad2fa17d71977c5ead4
peter zettl <mpzettl@gmail.com>
2018-06-26 17:52:45 -0500

    Jobs html works for All fixing else

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -4,17 +4,42 @@ using TechJobs.Models;
 
 namespace TechJobs.Controllers
 {
-    public class SearchController : Controller
-    {
-        public IActionResult Index()
-        {
-            ViewBag.columns = ListController.columnChoices;
-            ViewBag.title = "Search";
-            return View();
-        }
+	public class SearchController : Controller
+	{
 
-        // TODO #1 - Create a Results action method to process 
-        // search request and display results
+		public IActionResult Index()
+		{
+			ViewBag.columns = ListController.columnChoices; 
+			ViewBag.title = "Search";
+			return View();
+		}
 
-    }
+		// TODO #1 - Create a Results action method to process 
+		// search request and display results
+		
+		internal IActionResult Results(JobData FindAll)
+		{
+			ViewBag.columns = ListController.columnChoices;
+			ViewBag.title = "Search";
+
+			foreach (var searchType in ViewBag.columns)
+			{
+				foreach (var searchTerm in searchType)
+				if ((searchType.Equals("all")) || (searchTerm.Equals("")))
+				{
+					ViewBag.columns = JobData.FindByValue(searchTerm);
+				}
+				else
+				{
+					ViewBag.columns = JobData.FindByColumnAndValue(searchType, searchTerm);
+				}
+			}
+			return View();
+		}
+	}
+	
 }
+		
+
+	
+
